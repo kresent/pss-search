c676674e5ae362a4bf80ccf963bbf0880a6cd43f
dmitriy <dmvasyuk@gmail.com>
2019-09-08 22:48:50 +0300

    fix deploy

--- src/Market/utils.js
+++ src/Market/utils.js
@@ -1,7 +1,7 @@
 export function marketAdapter(entries) {
   return entries.map(({ _attributes: e }) => {
     const [currency, price] = e.ActivityArgument.split(':');
-    const message = e.Message.replace(/Vendre |Selling |Продает |Verkauf |Prodává |ขาย |판매|販售中 |중 |販售中/g, "").split(' ');
+    const message = e.Message.replace(/Vendre |Selling |Продает |Verkauf |Prodává |在卖|Venda|ขาย |판매|販售中 |중 |販售中/g, "").split(' ');
     const [quantity, ...goods] = message;
 
     return {
